cd3c29b39c3d2269f08cc3a66e5fb27c6f8bfd4e
Josh Suson <josh.suson@gmail.com>
2022-02-11 15:01:20 -0500

    Adding form from cognito

---
+++ app/routes/survey.jsx
@@ -0,0 +1,11 @@
+export default function Survey() {
+  return (
+    <div>
+      <script
+        src="https://www.cognitoforms.com/f/seamless.js"
+        data-key="VIFwyLJpI0-1pUDbhQ8wSg"
+        data-form="68"
+      ></script>
+    </div>
+  );
+}
